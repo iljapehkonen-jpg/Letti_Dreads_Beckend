9a712da08ddbfe558b3a65ffa275459060e80cf8
iljapehkonen <violy@list.ru>
2025-12-31 11:55:31 +0200

    add remove cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
     path('<int:user_id>', views.user_cart, name='user_cart'),
+    path('add/<int:product_id>/<int:user_id>', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:product_id>/<int:user_id>', views.remove_from_cart, name='remove_from_cart'),
 ]
